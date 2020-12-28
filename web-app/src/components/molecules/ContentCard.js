79b591b6d75b419b91105c5dc2570b41838074e2
funnise <motoifunase@gmail.com>
2020-12-28 15:31:46 +0900

    feat:マイメニューにコンテンツカードコンポーネントを追加

--- web-app/src/components/molecules/ContentCard.js
+++ web-app/src/components/molecules/ContentCard.js
@@ -11,6 +11,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import AddIcon from '@material-ui/icons/Add';
 
 export const AddContentCard = (props) => {
     const name = props.name;
@@ -19,20 +20,17 @@ export const AddContentCard = (props) => {
     const icons = props.icons ? props.icons : [];
     const onClick = props.onClick ? props.onClick : ()=>{};
     return(
-       <Card>
-        <Div>
-            <NamedAvatar avatar={avatar} name={name} caption={caption}/>
-        </Div>
-        <Hr/>
-        <ButtonWrap>
-            <Icons>
-                {icons.map((item,index) => (
-                    <IconButton key={index} href={item.href}>{item.icon}</IconButton>
-             ))}
-            </Icons>
-            <SmallButton onClick={onClick}>Edit</SmallButton>
-        </ButtonWrap>    
-       </Card>
+       <AddCardWrap>
+         <CardActionArea>
+        <StyledAddCard>
+          <DivAdd>
+            <DivHorizontal/>
+            <DivVertical/>
+          </DivAdd>
+        </StyledAddCard>
+        </CardActionArea>
+        <ButtonWrap>{'動画を追加する'}</ButtonWrap>
+       </AddCardWrap>
     );
 };
 
@@ -46,12 +44,12 @@ export const ContentCard = (props) => {
     const onClick = props.onClick ? props.onClick : ()=>{};
     return(
        <StyledCard>
-        <StyledCardMedia 
+        <CardActionArea>
+        <StyledCardMedia
         image='https://image.jimcdn.com/app/cms/image/transf/dimension=origxorig:format=jpg/path/s9952fd50fa8c415e/image/i583e2af84568bc57/version/1405748325/%E7%84%BC%E3%81%8D%E3%81%9F%E3%81%A6%E6%89%8B%E4%BD%9C%E3%82%8A%E3%83%91%E3%83%B3%E3%81%A8%E7%84%BC%E3%81%8D%E8%8F%93%E5%AD%90%E3%81%AE%E3%81%8A%E5%BA%97-%E3%83%91%E3%83%B3%E5%B7%A5%E6%88%BF%E3%83%B4%E3%82%A3%E3%82%A8%E3%83%8E%E3%83%AF.jpg'
         title="Contemplative Reptile"
         />
-        <CardActionArea>
-        <CardContent>
+        <StyledCardContent>
           <Typography gutterBottom variant="h5" component="h2">
             Lizard
           </Typography>
@@ -59,8 +57,8 @@ export const ContentCard = (props) => {
             Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
             across all continents except Antarctica
           </Typography>
-        </CardContent>
-        </CardActionArea> 
+        </StyledCardContent>
+        </CardActionArea>
        </StyledCard>
     );
 };
@@ -72,31 +70,107 @@ const StyledCard = styled(Card)`
     @media screen and (min-width: ${props => props.theme.breakpoints.values.sm}px){
         width:250px;
     }
-`
+`;
+
+const StyledAddCard = styled(Card)`
+    width:153px;
+    height: 108px;
+    border: 3px solid ${props => props.theme.palette.primary.main};
+    box-sizing: border-box;
+    box-shadow: 0px 7px 16px -3px rgba(0, 0, 0, 0.35);
+    border-radius: 4px;
+    text-algin: center;
+    @media screen and (min-width: ${props => props.theme.breakpoints.values.sm}px){
+        width:250px;
+        height: 176px;
+    }
+`;
 
 const StyledCardMedia = styled(CardMedia)`
-    width:153px;   
+    width:153px;
     height : 108px;
+    box-shadow: 0px 7px 16px -3px rgba(0, 0, 0, 0.35);
+    border-radius: 4px;
 @media screen and (min-width: ${props => props.theme.breakpoints.values.sm}px){
     width:250px;
     height:176px;
 }
 `
-
-const Div = styled.div`
-    display:flex;
-    flex-wrap: wrap;
-    flex-direction:column;
-    @media screen and (min-width: ${props => props.theme.breakpoints.values.sm}px){
-        flex-direction:row;
-    }
+const StyledCardContent = styled(CardContent)`
+  padding: 0;
+  width: 100%;
 `
 
-const ButtonWrap = styled.div`
-    display: flex;
+const AddCardWrap = styled.div`
+margin:${props => props.theme.spacing(2)}px ${props => props.theme.spacing(2)}px 0;
+width:153px;
+text-align: center;
+height: auto;
+box-shadow: none;
+@media screen and (min-width: ${props => props.theme.breakpoints.values.sm}px){
+    width:250px;
+}
+`
+
+const DivAdd = styled.div`
+position:relative;
+margin: 32px auto;
+width:35px;
+height: 35px;
+text-align: center;
+height: auto;
+box-shadow: none;
+@media screen and (min-width: ${props => props.theme.breakpoints.values.sm}px){
+  width:57px;
+  height: 57px;
+  margin:59px auto;
+}
+`
+const DivHorizontal = styled.div`
+position: absolute;
+width:100%;
+top: 17.5px;
+border: 1px solid #000000;
+@media screen and (min-width: ${props => props.theme.breakpoints.values.sm}px){
+    width:100%;
+    top: 28.5px;
+}
+`
+const DivVertical = styled.div`
+height: 35px;
+width:0;
+border: 1px solid #000000;
+text-align: center;
+margin: 0 auto;
+@media screen and (min-width: ${props => props.theme.breakpoints.values.sm}px){
+  width:0;
+  height: 57px;
+
+}
+`
+
+
+
+
+const ButtonWrap = styled(Button)`
+    width:auto;
+    text-algin: center;
+    font-weight: 700;
     justify-content: space-between;
     align-items: center;
-    margin-left:${props => props.theme.spacing(1)}px
+    color: ${props => props.theme.palette.primary.main};
+    background: none;
+    border-radius: 4px;
+    margin-top:${props => props.theme.spacing(2)}px;
+    @media screen and (min-width: ${props => props.theme.breakpoints.values.sm}px){
+      color: white;
+      background:${props => props.theme.palette.primary.main};
+      margin-top:${props => props.theme.spacing(3)}px;
+      padding: 3px 22px 3px;
+      &:hover {
+        background:${props => props.theme.palette.primary.light};
+      },
+  }
 `;
 
 const Icons = styled.span`
--- web-app/src/components/pages/MyMenuPage.js
+++ web-app/src/components/pages/MyMenuPage.js
@@ -1,33 +1,34 @@
 import React from 'react';
-import {FullPaper} from '../atoms/Paper';
+import {StyledFullPaper} from '../atoms/Paper';
 import {PageTitle} from '../atoms/Title';
 import {BasicTabs as Tabs} from '../molecules/Tabs';
 import { MyMenuAvatar } from '../atoms/Avatar';
 import { ContentCard } from '../molecules/ContentCard';
+import { ContentCardList } from '../molecules/ContentCardList';
 
 const MyMenuPage = () => {
   const contents = [
     {
       label:"タブ１",
-      content: <div style={{display: 'flex'}}><ContentCard/><ContentCard/></div>
+      content: <ContentCardList/>
     },
     {
       label:"タブ2",
-      content:<div>タブ２</div>
+      content: <ContentCardList/>
     }
   ]
   return(
     <div>
         <PageTitle>マイメニュー</PageTitle>
-        <FullPaper>
+        <StyledFullPaper>
         <MyMenuAvatar
           title={'プロバイダー'}
           subtitle1={'サブタイトル1'}
           subtitle2={'サブタイトル2'}
         />
-        <Tabs   
+        <Tabs
           contents={contents}/>
-        </FullPaper>
+        </StyledFullPaper>
     </div>
     );
 }
